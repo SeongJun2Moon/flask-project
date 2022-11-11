5ab7da12bba0b2788d4d63a8f9e0eb7b81334aa4
SJMoon <103993019+CentiHei@users.noreply.github.com>
2022-11-12 03:17:30 +0900

    또 잠을 설치고

--- titanic/views.py
+++ titanic/views.py
@@ -1,7 +1,10 @@
 from titanic.models import TitanicModel
 from util.dataset import Dataset
 
+
 class TitanicController(object):
+
+
     def __init__(self):
         pass
 
@@ -11,41 +14,48 @@ class TitanicController(object):
     dataset = Dataset()
     model = TitanicModel()
 
-    def mining(self):
-        pass
-    def preprocess(self, train, test) -> object:  # 전처리
+
+    def preprocess(self, train, test) -> object: # 전처리
         model = self.model
         this = self.dataset
         this.train = model.new_model(train)
         this.test = model.new_model(test)
         this.id = this.test['PassengerId']
         # columns 편집과정
-        # this = model.pclass_ordinal(this) # 수정없이 순서만 정하면 됨
-        this = model.sex_norminal(this)
+        # this = model.pclass_ordinal(this) 데이터자체가 이미 오디널이다.
+        this = model.sex_norminal(this) # 프로토 타입
         this = model.age_ordinal(this)
         this = model.fare_ordinal(this)
-        this = model.embarked_nominal(this) # 내부 스칼라의 props가 바귐
+        this = model.embarked_norminal(this)
         this = model.title_norminal(this)
-        this = model.drop_features(this, "PassengerId", "Name", "Sex", "Age",
-                                   "SibSp", "Parch", "Ticket", "Fare", "Cabin")
+        this = model.drop_features(this, 'PassengerId','Name', 'Sex', 'Age', 'SibSp',
+                                   'Parch', 'Ticket', 'Fare', 'Cabin')
+
         return this
 
-    def modeling(self, train, test) -> object:  # 모델생성
+    def modeling(self, train, test) -> object: # 모델생성
         model = self.model
         this = self.preprocess(train, test)
-        this.label = model.create_label(this) # 답안지
+        this.label = model.create_label(this)
         this.train = model.create_train(this)
         return this
 
-    def learning(self):  # 기계학습
+    def learning(self, train, test, algo): # 머신러닝
+        model = self.model
+        this = self.modeling(train, test)
+        accuracy = model.get_accuracy(this, algo[0])
+        accuracy2 = model.get_accuracy(this, algo[1])
+        accuracy3 = model.get_accuracy(this, algo[2])
+        print(f"랜덤포레스트 분류기 : {accuracy}%\n"
+              f"결정트리분류기: {accuracy2} %\n"
+              f"로지스틱회귀: {accuracy3}")
+
+    def submit(self): # 배포
         pass
 
-    def submit(self):  # 배포
-        pass
-
-if __name__ == '__main__':
-    tc = TitanicController()
-    this = tc.modeling('train.csv',"test.csv")
-    print(this.train.head(6))
+if __name__=="__main__":
+    t = TitanicController()
+    this = Dataset()
+    this = t.modeling('train.csv', 'test.csv')
+    print(this.train.head())
     print(this.train.columns)
-    print(this.train.isnull)
